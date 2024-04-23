382f7a6091b79dd1d8a41538a1b8356a0b1287d7
tomiliudev <tomiliudev@yahoo.co.jp>
2024-04-23 22:48:24 +0900

    ルーティング設定

--- mugendouShopping/app/Http/Controllers/owner/ProfileController.php
+++ mugendouShopping/app/Http/Controllers/owner/ProfileController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Owner;
 
 use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
--- mugendouShopping/routes/web.php
+++ mugendouShopping/routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\user\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user/welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('user/dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -17,4 +17,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
