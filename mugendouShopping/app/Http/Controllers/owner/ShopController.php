0e0abe8c3b602e31b02040f14e11d8eaedf10cc8
tomiliudev <tomiliudev@yahoo.co.jp>
2024-05-12 22:49:23 +0900

    intervention imageの導入＆ショップ画像をサーバー側でリサイズするように変更

--- mugendouShopping/app/Http/Controllers/owner/ShopController.php
+++ mugendouShopping/app/Http/Controllers/owner/ShopController.php
@@ -10,6 +10,7 @@ use Illuminate\View\View;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
 
 class ShopController extends Controller implements HasMiddleware
 {
@@ -35,7 +36,6 @@ class ShopController extends Controller implements HasMiddleware
 
     public function index()
     {
-        phpinfo();
         $ownerId = Auth::id();
         $shopList = Shop::where('ownerId', $ownerId)->get();
         return view('owner.shop.index', compact('shopList'));
@@ -51,7 +51,12 @@ class ShopController extends Controller implements HasMiddleware
     {
         $shopImage = $request->image;
         if (!is_null($shopImage) && $shopImage->isValid()) {
-            Storage::putFile('public/shop/', $shopImage);
+            // Storage::putFile('public/shop/', $shopImage); // ファイルオブジェクト
+            $resizedImage = ImageManager::gd()->read($shopImage)->resize(1920, 1080)->encode();
+            $fileName = uniqid(rand() . '_');
+            $extension = $shopImage->extension();
+            $uniqFileName = $fileName . '.' . $extension;
+            Storage::put('public/shop/' . $uniqFileName, $resizedImage);
         }
 
         return redirect()->route('owner.shop.index');
