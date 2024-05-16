6c48d6f71280b87843e0aaa40c7613d0f341fd2e
tomiliudev <tomiliudev@yahoo.co.jp>
2024-05-16 22:29:21 +0900

    プロダクトの画像一覧画面でmd以上なら一行４枚、それ以下の場合は一行２枚で表示されるように調整

--- mugendouShopping/resources/views/owner/image/index.blade.php
+++ mugendouShopping/resources/views/owner/image/index.blade.php
@@ -24,9 +24,9 @@
                             @else
                                 <div class="flex flex-wrap">
                                     @foreach ($images as $image)
-                                        <div class="w-1/4 p-4">
+                                        <div class="w-1/2 md:w-1/4 p-2">
                                             <a href="{{route('owner.images.edit', ['image' => $image->id])}}">
-                                                <div class="border rounded-md p-4">
+                                                <div class="border rounded-md p-2">
                                                     <div class="text-xl">
                                                         {{$image->title;}}
                                                     </div>
